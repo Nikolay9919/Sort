d842452ffbb19b5c139e5edc5617381468b1bdf5
Nik <TEST@TEST.COM>
2017-06-20 12:25:32 +0300

    java code convention

--- src/main/java/com/mycompany/sort/Main.java
+++ src/main/java/com/mycompany/sort/Main.java
@@ -7,17 +7,17 @@ public class Main {
         int[] arr = {32, 21, 454, 65, 12};
         for (int i = 0; i < arr.length; i++) {
             int max = arr[i];
-            int max_i = i;
+            int maxI = i;
             for (int j = i + 1; j < arr.length; j++) {
                 if (arr[j] > max) {
                     max = arr[j];
-                    max_i = j;
+                    maxI = j;
                 }
             }
-            if (i != max_i) {
+            if (i != maxI) {
                 int tmp = arr[i];
-                arr[i] = arr[max_i];
-                arr[max_i] = tmp;
+                arr[i] = arr[maxI];
+                arr[maxI] = tmp;
             }
         }
         for (int i = 0; i < arr.length; i++) {
